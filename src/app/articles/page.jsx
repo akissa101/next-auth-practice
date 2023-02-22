3b8bc3ed9256783c75d07afa4f4f1c2b04810966
Abdikarim Issa <kareemorbit@gmail.com>
2023-02-22 12:58:10 +0300

    complete fetch articles

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -1,24 +1,24 @@
 "use client";
-import { useGetUsers } from "@/hooks/useUserData";
+import { useGetArticles } from "@/hooks/useArticleData";
 
 export default function ArticlesList() {
-  const { data: users } = useGetUsers();
+  const { data: articles } = useGetArticles();
 
   return (
     <div className="h-screen mt-4">
       <h1 className="text-3xl p-4 font-bold font-sans">Articles</h1>
 
       <div className="grid grid-cols-4 text-lg font-bold  p-2 border-b border-slate-300 dark:border-slate-700">
-        <div className="">User</div>
+        <div className="">Article</div>
         <div className="">Title</div>
         <div className="">Content</div>
         <div className="">Actions</div>
       </div>
-      {users?.data.map((user) => (
-        <div key={user.id} className="grid  grid-cols-4 p-2">
-          <div className="">{user.username}</div>
-          <div className="">{user.name}</div>
-          <div className="">{user.email}</div>
+      {articles?.data.map((article) => (
+        <div key={article._id} className="grid  grid-cols-4 p-2">
+          <div className="">{article.userid}</div>
+          <div className="">{article.title}</div>
+          <div className="">{article.content}</div>
           <div className="">Edit/Delete</div>
         </div>
       ))}
---
+++ src/models/Article.jsx
@@ -0,0 +1,20 @@
+import mongoose, { model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const articleSchema = new mongoose.Schema(
+  {
+    userid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Article ||
+  mongoose.model("Article", articleSchema);
